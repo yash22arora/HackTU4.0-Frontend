f80abd0350607ffa89a1d9c6fb2046b8a1a19791
Yashvardhan Arora <yash22arora@gmail.com>
2023-02-04 17:12:22 +0530

    Setup initial repo
    Signed-off-by: Yashvardhan Arora <yash22arora@gmail.com>

---
+++ client/app/routes/index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div className="flex w-screen h-screen items-center justify-center p-8">
+      <div className="max-w-[400px] w-full bg-[#191919] h-full text-white p-2">
+        <h1 className="text-2xl">Test</h1>
+      </div>
+    </div>
+  );
+}
